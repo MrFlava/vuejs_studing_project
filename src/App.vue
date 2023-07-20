a4c11095ef702400b544703d63fa400a90068fa1
MrFlava <thatelitemaili33t@gmail.com>
2023-07-20 11:35:20 +0300

    Feat: provide HelloWorld component, but need to fix the bug

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
 import Click_counter from "@/components/Click_counter.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+
+// const hello_world = HelloWorld()
+// export default {
+//   name: "app",
+//   components: {
+//     hello_world
+//   }
+// };
 </script>
 
 <template>
@@ -7,5 +17,6 @@ import Click_counter from "@/components/Click_counter.vue";
     <h1>Here is a child component!</h1>
   </header>
   <Click_counter />
+  <HelloWorld />
 
 </template>
