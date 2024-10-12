7906158e2b6119ba8c33c9399860b355d4f86ee5
MrFlava <thatelitemaili33t@gmail.com>
2024-10-12 15:30:52 +0300

    Click events part (lssn1)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,8 +13,31 @@ defineProps({
     <h3>
       Yes It's working
     </h3>
+    <p>{{ age }}</p>
+    <p>{{title}}</p>
+    <div id="click-event">
+      <button v-on:click="age++">Add stuff</button>
+      <button v-on:click="age--">Remove stuff</button>
+      <div @click="changeTitle('interesting')">Change message</div>
+
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      "title": "hey",
+      "age": 100,
+    }
+  },
+  methods: {
+    changeTitle(title){
+      this.title = title
+    }
+  }
+}
+</script>
 
 <style scoped>
 </style>
