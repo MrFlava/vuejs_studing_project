574edf7567c266709396b8dfc75bb46adfdae8d2
MrFlava <thatelitemaili33t@gmail.com>
2024-10-22 21:41:55 +0300

    Remove old components and styles, start to work on the clicker

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Modal from "@/components/Modal.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <p>Welcome...</p>
-      <teleport to=".modals" v-if="showModal">
-        <Modal theme="sale" @close="toggleModal">
-          <template v-slot:links>
-            <a href="https://google.com">Sign up now</a>
-            <br>
-            <a href="https://google.com">More info</a>
-          </template>
-          <h1>Sign up for the Giveavay</h1>
-          <p>Grab the suit for the halfprice</p>
-        </Modal>
-      </teleport>
-    </div>
-    <button @click.shift="toggleModal">Open modal (shift)</button>
-  </header>
-
-  <main>
-  </main>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start">Play</button>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      showModal: false
+      isPlaying: false,
+      delay: null
     }
   },
   methods: {
-    toggleModal(){
-      this.showModal = !this.showModal;
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay)
     }
   }
 }
