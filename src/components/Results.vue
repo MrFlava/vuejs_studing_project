43860069ea69f2f493b6d6ce68b512e7f21b7168
MrFlava <thatelitemaili33t@gmail.com>
2024-10-26 23:48:29 +0300

    Work on the result ranking in the Results.vue component

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -8,9 +8,33 @@ defineProps({
 </script>
 
 <template>
-  <p>Reaction time - {{score}}</p>
+  <p>Reaction time - {{score}} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250){
+      this.rank = 'Ninja fingers'
+    } else if (this.score < 400){
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail pace...'
+    }
+  }
+}
+</script>
 
+<style scoped>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
 </style>
