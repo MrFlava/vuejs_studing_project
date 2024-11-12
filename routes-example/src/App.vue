8146080b5bb18309b1038067948ba9100fff2552
MrFlava <thatelitemaili33t@gmail.com>
2024-11-12 20:56:41 +0200

    Folder structure

--- routes-example/src/App.vue
+++ routes-example/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -9,6 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/jobs">Jobs</RouterLink>
       </nav>
     </div>
   </header>
---
+++ routes-example/src/views/jobs/JobsView.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{job.title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        {title: 'Test', id: 1, details: "lorem"},
+        {title: 'Test1', id: 2, details: "lorem"},
+        {title: 'Test2', id: 3, details: "lorem"}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
