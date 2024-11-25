ff39fa8b1d4af4fc9849c6b20d8bbc0965cea1db
MrFlava <thatelitemaili33t@gmail.com>
2024-11-25 19:31:17 +0200

    Composition API basics, delete unused components, fix route in last app

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Home",
+  setup() {
+    console.log('setup')
+
+    let name = 'mairo'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('clicked me')
+    }
+
+    return{name, age, handleClick}
+  }
+}
+</script>
