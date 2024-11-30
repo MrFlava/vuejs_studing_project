08d22fda7e3c5abdd734ee6a70f61523b1229254
MrFlava <thatelitemaili33t@gmail.com>
2024-11-30 19:12:01 +0200

    Composition API basics: refs and reactive

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -3,41 +3,50 @@
 
 <template>
   <div class="home">
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name" />
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
+    <!--    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>-->
+<!--    <button @click="handleClick">Click me</button>-->
+<!--    <button @click="age++">add 1 to age</button>-->
+<!--    <input type="text" v-model="name" />-->
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
-  name: "Home",
+  // name: "Home",
   setup() {
-    console.log(this)
+    const ninjaOne = ref({name: 'mario', age: 12})
+    const ninjaTwo = reactive({name: 'maka', age: 24})
 
-    const p = ref(null)
-
-    const name =ref('mairo')
-    const age = ref(30)
-
-    const handleClick = () => {
-      name.value = 'luigi'
-      age.value = 45
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40
+    }
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 50
+    }
+    // console.log(this)
+    //
+    // const p = ref(null)
+    //
+    // const name =ref('mairo')
+    // const age = ref(30)
+    //
+    // const handleClick = () => {
+    //   name.value = 'luigi'
+    //   age.value = 45
       // p.value.classList.add('test')
       // console.log(p, p.value)
       // p.value.textContent = 'helloooooooooooaaa'
-    }
-
-    return{name, age, handleClick, p}
-  },
-
-  data() {
-    return {
-      score: 5
-    }
+    return {ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameTwo}
+    // return{name, age, handleClick, p}
   }
 }
 </script>
